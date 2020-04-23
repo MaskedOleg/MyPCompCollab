6b98c42c2e9bbda542c080fbc334233aed085e7a
MaskedOleg <maskedangelo27@gmail.com>
2020-04-23 12:17:19 +0800

    Proto-gapFill working

--- gapFill.py
+++ gapFill.py
@@ -1,7 +1,7 @@
 import re
 
 textFiles = ['Item005.doc', 'Item001.doc', 'Item007.doc', 
-'File148.exl', 'File147.exl', 'txt30.os', 'txt25.os', 'txt27.os']
+'File150.exl', 'File147.exl', 'txt030.os', 'txt025.os', 'txt027.os']
 
 textFiles.sort()
 
@@ -10,15 +10,15 @@ fillRegex = re.compile(r'(.*?)(\d{1,})(.*)')
 # (.*?) all string that are not digits
 # (\d{1,}) matches onsecutive string of digits that are at length 1 or more
 # (.*?) mall strings after the digit
-counter = 0
+start = 0
 
 for items in textFiles:
     found = fillRegex.search(items).groups()
-    if counter is 0:
+    if start is 0:
         hold = found[0] # string 1
         pos = int(found[1]) # number
         ext = found[2]  # extension
-        counter = counter + 1
+        start = 1
     else:
         newhold = found[0] # string 1
         newpos = int(found[1]) # number
@@ -26,9 +26,14 @@ for items in textFiles:
         if hold == found[0]:
             while newpos != pos + 1:
                 newpos = newpos - 1
-            counter = 0
+            pos = pos + 1
             textFiles.remove(items)
+            newName = newhold + str(newpos).zfill(3) + newext
+            textFiles.append(newName)
+            textFiles.sort()
         else:
-            continue
-
-#print(found[0])
+            hold = found[0] # string 1
+            pos = int(found[1]) # number
+            ext = found[2]  # extension
+for items in textFiles:
+    print(items)
