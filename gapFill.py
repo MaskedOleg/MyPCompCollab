51ad24382d3dfd399f5a3064a4401543f63feac9
MaskedOleg <maskedangelo27@gmail.com>
2020-04-24 16:48:34 +0800

    gapFill.py now working as intended

--- gapFill.py
+++ gapFill.py
@@ -1,8 +1,8 @@
-import re
+import re, os, shutil
 
-textFiles = ['Item005.doc', 'Item001.doc', 'Item007.doc', 
-'File150.exl', 'File147.exl', 'txt030.os', 'txt025.os', 'txt027.os']
-
+PATH = 'D:\Python 3\Projects and Self-Exercise\Chapter 9 - Filling in the Gaps'
+os.chdir(PATH)
+textFiles = os.listdir()
 textFiles.sort()
 
 fillRegex = re.compile(r'(.*?)(\d{1,})(.*)')
@@ -12,28 +12,32 @@ fillRegex = re.compile(r'(.*?)(\d{1,})(.*)')
 # (.*?) mall strings after the digit
 start = 0
 
-for items in textFiles:
-    found = fillRegex.search(items).groups()
-    if start is 0:
-        hold = found[0] # string 1
-        pos = int(found[1]) # number
-        ext = found[2]  # extension
-        start = 1
-    else:
-        newhold = found[0] # string 1
-        newpos = int(found[1]) # number
-        newext = found[2]  # extension
-        if hold == found[0]:
-            while newpos != pos + 1:
-                newpos = newpos - 1
-            pos = pos + 1
-            textFiles.remove(items)
-            newName = newhold + str(newpos).zfill(3) + newext
-            textFiles.append(newName)
-            textFiles.sort()
-        else:
+for items in textFiles:    
+    try:
+        found = fillRegex.search(items).groups()
+        if start is 0:
             hold = found[0] # string 1
             pos = int(found[1]) # number
             ext = found[2]  # extension
+            start = 1
+        else:
+            newhold = found[0] # string 1
+            newpos = int(found[1]) # number
+            newext = found[2]  # extension
+            if hold == found[0]:
+                while newpos != pos + 1:
+                    newpos = newpos - 1
+                pos = pos + 1
+                newName = newhold + str(newpos).zfill(3) + newext
+                shutil.move(items, newName)
+                textFiles.remove(items)
+                textFiles.append(newName)
+                textFiles.sort()
+            else:
+                hold = found[0] # string 1
+                pos = int(found[1]) # number
+                ext = found[2]  # extension
+    except AttributeError:
+        continue
 for items in textFiles:
     print(items)
